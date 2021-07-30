4a05d3b8bac77c914687f45892e38dd99f61df00
sowmya.matsa <sowmya.matsa@gmail.com>
2021-07-30 19:29:31 +0530

    small change

--- youtube/settings.py
+++ youtube/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!e$&cnd&84omv5te3x59k0rr-ypjx&@decsh)yxa*a0z46x$w7
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-*ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
